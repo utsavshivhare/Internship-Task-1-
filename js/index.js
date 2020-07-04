061f0e170804b61c42ffb68b7680ccce2897d0c6
utsavshivhare <utsavshivhare946@gmail.com>
2020-07-04 22:39:00 +0530

    my first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function java(){
+		document.getElementById("demo").innerHTML="are in demand today.";
+	}
+	// function likejs(){
+	// 	document.getElementById("likejava").src="images/likejava.jpg";
+	// }
